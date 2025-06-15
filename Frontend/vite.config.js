ba901272fe3a4d940f0a4b6bee7d4d1f4e26b1af
Abdul-Samed <82516089+Sumaila-dev@users.noreply.github.com>
2025-06-15 23:27:51 +0000

    frontend dockerfile

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,4 +4,22 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 80,
+    proxy: {
+      '/api': {
+        target: 'http://backend:3000',
+        changeOrigin: true,
+      },
+      '/todos': {
+        target: 'http://backend:3000',
+        changeOrigin: true,
+      }
+    }
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 80,
+  }
 })
